3080e87d3b14dfc9547d48477f5a310c01dd7653
Benjamin Kober <benko@kober-systems.com>
2024-01-16 11:19:41 +0100

    First test for redemensioned pictures

--- tests/test_compare.rs
+++ tests/test_compare.rs
@@ -9,6 +9,14 @@ fn when_the_images_are_equal_return_empty() {
     );
 }
 
+#[test]
+fn when_the_picture_is_redemensioned_return_difference() {
+    image_compare(
+        "./tests/difference_redemensioned.png",
+        compare_images_from_path("./tests/square.png", "./tests/redemensioned.png"),
+    );
+}
+
 fn image_compare(original: &str, compared: DynamicImage) {
     let original_img = open(original).expect(&format!("Failed to open file {}", original));
 

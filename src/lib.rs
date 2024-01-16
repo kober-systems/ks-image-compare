e371e8977c164b9a8bc7f7e93eee78781f42bc65
Benjamin Kober <benko@kober-systems.com>
2024-01-16 10:29:20 +0100

    Implemented first image test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+use image::open;
 use image::DynamicImage;
 
 fn compare_images(original: DynamicImage, compared: DynamicImage) -> DynamicImage {
     DynamicImage::default()
+}
+
+pub fn compare_images_from_path(original: &str, compared: &str) -> DynamicImage {
+    let original_img = open(original).expect(&format!("Failed to open file {}", original));
+    let compared_img = open(compared).expect(&format!("Failed to open file {}", compared));
+    compare_images(original_img, compared_img)
 }
 
 #[cfg(test)]
--- tests/test_compare.rs
+++ tests/test_compare.rs
@@ -2,13 +2,15 @@ use image::{open, DynamicImage};
 use image_compare::*;
 
 #[test]
-fn basic_test() {
-    assert_eq!(1, 1);
+fn when_the_images_are_equal_return_empty() {
+    image_compare(
+        "./tests/empty.png",
+        compare_images_from_path("./tests/square.png", "./tests/square.png"),
+    );
 }
 
-fn image_compare(original: &str, compared: &str) {
+fn image_compare(original: &str, compared: DynamicImage) {
     let original_img = open(original).expect(&format!("Failed to open file {}", original));
-    let compared_img = open(compared).expect(&format!("Failed to open file {}", compared));
 
-    assert_eq!(original_img, compared_img);
+    assert_eq!(original_img, compared);
 }

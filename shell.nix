979e13abe1f37b6cc301db032474aa4afa39b760
Benjamin Kober <benko@kober-systems.com>
2024-01-23 11:14:18 +0100

    Fixed environment to work with egui

--- shell.nix
+++ shell.nix
@@ -4,11 +4,27 @@ let
   pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
 in
 pkgs.mkShell {
-  nativeBuildInputs = [
-    pkgs.rust-bin.stable.latest.${profile}
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+    libGL
+    libxkbcommon
+    wayland
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+  ];
+  nativeBuildInputs = with pkgs; [
+    rust-bin.stable.latest.${profile}
+
+    cmake
+    pkg-config
+    fontconfig
+    libxkbcommon
+    openssl
+    xorg.libxcb
 
     # keep this line if you use bash
-    pkgs.bashInteractive
+    bashInteractive
   ];
 }
 

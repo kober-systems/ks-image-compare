233d39308519a13d10b274fafd2b72d67b448349
Benjamin Kober <benko@kober-systems.com>
2024-02-06 13:06:07 +0100

    Removed obsolete import

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use std::{fmt::format, path::PathBuf};
-
 use eframe::egui;
 use egui::ColorImage;
 use image::DynamicImage;
